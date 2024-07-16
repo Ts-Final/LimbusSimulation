ae430fccea60ecac9a8ced4f58423b428be1a8fd
Ts-Final <chara_x@126.com>
2024-07-16 21:08:59 +0800

    update workflow 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => {
-  if (command == 'serve') {
-    return {
-      plugins: [vue()],
-      base: "/",
-    }
-  } else {
-    return {
-      plugins: [vue()],
-      base: "https://ts-final.github.io/LimbusSimulation"
-    }
-  }
+export default defineConfig({
+  plugins: [vue()],
+  base: "./"
 })
